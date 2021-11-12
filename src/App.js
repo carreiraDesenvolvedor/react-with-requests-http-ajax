ae701f33a7007e3f50eaf6fabf51ca3908f035f1
Jonathan Melo <desenvolvedor.jonathan@gmail.com>
2021-11-12 16:21:47 +0000

    Construindo o menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Usuarios from './components/Usuarios/Usuarios'
+import Usuarios from "./components/Usuarios/Usuarios";
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Início</a>
+            </li>
+            <li>
+              <a href="/users">Usuários</a>
+            </li>
+            <li>
+              <a href="/add">Adicionar Usuário</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <main>
         <Usuarios />
       </main>
