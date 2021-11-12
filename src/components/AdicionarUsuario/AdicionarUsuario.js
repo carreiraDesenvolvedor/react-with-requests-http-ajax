8304397293ab6f19da41fd8e60b856b09669e3f9
Jonathan Melo <desenvolvedor.jonathan@gmail.com>
2021-11-12 16:08:41 +0000

    Adiocionado requests, adicionado hooks

---
+++ src/components/AdicionarUsuario/AdicionarUsuario.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react'
+
+import './AdicionarUsuario.css'
+
+const AdicionarUsuario = (props) => {
+
+    const [name, setName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault()
+            // const id = Math.floor(Math.random() * 1000)
+        const usuario = {
+            nome: name,
+            sobrenome: lastName,
+            email
+        };
+
+        fetch('https://reqres.in/api/users', {
+                method: 'POST',
+                headers: { 'Content-type': 'application/json' },
+                body: JSON.stringify(usuario)
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setName('');
+                setLastName('');
+                setEmail('');
+                props.adicionarUsuario(data)
+            })
+    }
+
+    return ( 
+        <div className = "AdicionarUsuario">
+            <h2> Adicionar Usuário </h2>  
+            <form onSubmit = { onSubmitHandler } >
+            <div className = "Linha">
+                <div className = "Coluna">
+                    <label> Nome </label> 
+                    <input type = "text"
+                        name = "nome"
+                        value = { name }
+                        onChange = { (event) => setName(event.target.value) }
+                        required />
+                </div> 
+                <div className = "Coluna">
+                    <label> Sobrenome </label> 
+                    <input type = "text"
+                        name = "sobrenome"
+                        value = { lastName }
+                        onChange = { (event)=>setLastName(event.target.value) }
+                        required />
+                </div>
+            </div> 
+            <div className = "Linha">
+                <div className = "Coluna" >
+                    <label> Email </label> 
+                    <input type = "email"
+                        name = "email"
+                        value = { email }
+                        onChange = { (event)=>setEmail(event.target.value) }
+                        required />
+                </div> 
+            </div> 
+            <button type = "submit" >Adicionar </button>  
+        </form> 
+    </div>
+    )
+}
+
+export default AdicionarUsuario
---
+++ src/components/Usuarios/Usuarios.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react'
+
+import AdicionarUsuario from '../AdicionarUsuario/AdicionarUsuario'
+import Usuario from '../Usuario/Usuario'
+
+const Usuarios = (props) => {
+
+  const [list, setList] = useState([]);
+
+  useEffect(()=>{
+    console.log("use effect...")
+    fetch('https://reqres.in/api/users')
+      .then((response) => {
+          return response.json()
+      })
+      .then((data) => {
+
+          console.log(data);
+          const users = data.data.map((user) => {
+              return {
+                  id: user.id,
+                  nome: user.first_name,
+                  sobrenome: user.last_name,
+                  email: user.email
+              }
+          })
+
+          setList(users);
+      })
+  },[]);
+
+  console.log('render');
+
+  const adicionarUsuario = (user) => {
+      setList([...list, user]);
+  }
+
+  const removerUsuario = (usuario) => {
+      if (window.confirm(`Tem certeza que deseja remover "${usuario.nome} ${usuario.sobrenome}"?`)) {
+        fetch(`https://reqres.in/api/users/${usuario.id}`,{
+          method:"DELETE"
+        }).then((response)=>{
+          if(response.ok){
+            setList(list.filter(x => x.id !== usuario.id));
+          }
+        })
+      }
+  }
+
+  return (<>
+      <AdicionarUsuario adicionarUsuario = {adicionarUsuario}/>
+        {
+            list.map(usuario => ( 
+              <Usuario 
+                key={ usuario.id }
+                usuario={ usuario }
+                removerUsuario={ removerUsuario.bind(this, usuario) } />
+            ))
+        } 
+      </>
+  )
+}
+
+export default Usuarios
